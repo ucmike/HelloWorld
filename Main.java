6158e220951f7d337e7cb83de839af22c0732638
Mike Seitz <seitzme@mail.uc.edu>
2021-01-17 15:08:00 +0000

    added information about variables

--- Main.java
+++ Main.java
@@ -1,6 +1,21 @@
+/* multiline comment space
+this is a multiline comment*/
 class Main {
   public static void main(String[] args) {
     System.out.println("Hello World!");
     System.out.println("My name is Mike Seitz");
+//this section is about integers
+    int num = 42;
+    System.out.println("num is " +num);
+
+    int num2, num3;
+    num2 = 5;
+
+    num3= num + num2;
+    System.out.println("num3 is " +num3);
+
+String s ="Hello! \n My name\'s Mike Seitz";
+System.out.println(s);
+
   }
 }
